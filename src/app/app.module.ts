d664eee025b12b1dce05dd59f68d1ced6d0fdb3c
unknown <vitorqt@hotmail.com>
2019-08-07 01:27:48 -0300

    Adiciona component main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'main', component: MainComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {
+      anchorScrolling: 'enabled',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
